97a7f7f71c85ead2fe8b157a073c79df0dd8c9a8
Srikanth Reddy <88000780+ssrikanthreddy@users.noreply.github.com>
2024-09-28 11:15:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import StepIndicator from "./components/StepIndicator.jsx";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainCard from "./Components/MainCard.jsx"; 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainCard from "./Components/MainCard.jsx";
 
 import "./App.css";
 
+
 function App() {
-    return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-cover bg-no-repeat">
-            {/* <StepIndicator /> */}
-            <MainCard />
-        </div>
-    );
+    
+    
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-cover bg-no-repeat">
+      <MainCard />
+    </div>
+  );
 }
 
 export default App;
