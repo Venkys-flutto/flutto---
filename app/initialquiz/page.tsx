07e4443e697f6abd28e506df6e2406b63b84b0b4
Beemagari Venkatesh <you@example.com>
2024-11-03 22:29:50 +0530

    Added PP1 component with first-time login redirect functionality

---
+++ app/initialquiz/page.tsx
@@ -0,0 +1,9 @@
+import PP1 from "@/components/questions/pp1";
+
+export default function InitialQuiz() {
+    return (
+        <div>
+            <PP1 />
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Templand from "@/components/templand";
-
+import PP1 from "@/components/questions/pp1";
 export default function Home() {
   return (
     <div>
