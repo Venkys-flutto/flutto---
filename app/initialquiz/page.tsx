e3aefd3c2d369399940a7ec67d2bdc431438b175
Venkatesh <114250014+Venkatbeemagari@users.noreply.github.com>
2024-11-14 18:59:16 +0530

    Delete app directory

--- app/initialquiz/page.tsx
+++
@@ -1,9 +0,0 @@
-import PP1 from "@/components/questions/pp1";
-
-export default function InitialQuiz() {
-    return (
-        <div>
-            <PP1 />
-        </div>
-    )
-}
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Templand from "@/components/templand";
-export default function Home() {
-  return (
-    <div>
-      <Templand />
-    </div>
-  );
-}
